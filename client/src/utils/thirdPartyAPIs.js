18475f5500577ecb723e23c46b3bd03a3fc7c2c6
Keith Naber <knaber4124@gmail.com>
2019-11-16 13:29:20 -0500

    updated API routes

---
+++ client/src/utils/thirdPartyAPIs.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export default thirdPartyAPI({
+    getWeather = () => {
+        let weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.weatherAPIKey}&q=${zipCode}`
+        $.ajax({
+            url: weatherAPIURL,
+            method: 'GET'
+        }).then(function (response) {
+            response.forEach(element => {
+                console.log(element);
+            });
+        })
+    },
+
+    getZipCode = () => {
+        let zipCodeAPIURL = `https://www.zipcodeapi.com/rest/${process.env.zipCodeAPIKey}/city-zips.json/${city}/${state}
+    `
+        $.ajax({
+            url: zipCodeAPIURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.zip_codes[0]);
+            let zipCode = response.zip_codes[0];
+        })
+    },
+
+    getZipCodeAndWeather = () => {
+        let zipCodeAPIURL = `https://www.zipcodeapi.com/rest/${process.env.zipCodeAPIKey}/city-zips.json/${city}/${state}
+    `
+        $.ajax({
+            url: zipCodeAPIURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.zip_codes[0]);
+            let zipCode = response.zip_codes[0];
+        }).then(function (res) {
+            getWeather();
+        })
+    }
+
+
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
@@ -17,6 +18,46 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/events', require('./routes/events'));
 app.use('/api/invite', require('./routes/invite'));
+
+
+getZipCode = () => {
+    let zipCodeAPIURL = `https://www.zipcodeapi.com/rest/${process.env.zipCodeAPIKey}/city-zips.json/${city}/${state}
+    `
+    $.ajax({
+        url: zipCodeAPIURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.zip_codes[0]);
+        let zipCode = response.zip_codes[0];
+    })
+};
+
+getZipCodeAndWeather = () => {
+    let zipCodeAPIURL = `https://www.zipcodeapi.com/rest/${process.env.zipCodeAPIKey}/city-zips.json/${city}/${state}
+    `
+    $.ajax({
+        url: zipCodeAPIURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.zip_codes[0]);
+        let zipCode = response.zip_codes[0];
+    }).then(function (res) {
+        getWeather();
+    })
+};
+
+
+getWeather = () => {
+    let weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.weatherAPIKey}&q=${zipCode}`
+    $.ajax({
+        url: weatherAPIURL,
+        method: 'GET'
+    }).then(function (response) {
+        response.forEach(element => {
+            console.log(element);
+        });
+    })
+};
 
 const PORT = process.env.PORT || 5000;
 
