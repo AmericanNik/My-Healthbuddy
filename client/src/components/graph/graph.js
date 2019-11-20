4c2da2b309ada43ac5e751d3edf827160eab3cdd
Keith Naber <knaber4124@gmail.com>
2019-11-19 23:45:31 -0500

    chart base setup still need to bring in data

--- client/src/components/graph/graph.js
+++ client/src/components/graph/graph.js
@@ -1,65 +1,88 @@
-import React from "react";
-import Chart from "chart.js";
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+
+class Graph extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: {
+                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+                datasets: [{
+                    label: "Severity",
+                    data: [// Severity Data from Database
+                        5, 6, 7, 8, 10, 1, 2, 5, 2, 4
+                    ],
+                    backgroundColor: "black",
+                    borderColor: "green",
+                    borderWidth: 3,
+                    fill: false
+
+                },
+                {
+                    label: "Activity Level",
+                    data: [
+                        //Activity Level Data from Datbase
+                        1, 2, 3, 6, 10, 4, 5, 7, 4, 9
+                    ],
+                    backgroundColor: "black",
+                    borderColor: "blue",
+                    borderWidth: 3,
+                    fill: false
+
+                },
+                {
+                    label: "Temperature",
+                    data: [
+                        //Temperature Data from Database
+                        6, 6, 6, 6, 6, 7, 8, 5, 10, 8
+                    ],
+                    backgroundColor: "black",
+                    borderColor: "red",
+                    borderWidth: 3,
+                    fill: false
+
+                },
+                {
+                    label: "Humidity",
+                    data: [
+                        // Humidity Data from Database
+                        1, 2, 1, 3, 4, 5, 2, 3, 4, 5
+                    ],
+                    backgroundColor: "black",
+                    borderColor: "purple",
+                    borderWidth: 3,
+                    fill: false
+
+
+                }]
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="graph">
+                <Line
+                    data={this.state.chartData}
+                    width={300}
+                    height={300}
+                    options={{
+                        maintainAspectRatio:false,
+                        title: {
+                            display: true,
+                            text: "Health Buddy Trends",
+                            fontSize:25
+                        }
+                    }}
+                />
+
+            </div>
+        )
+    }
+
+}
 
 
 
-function Graph(props) {
-    return (
-        <div>
-            new Chart(cty, {
-                type: 'line',
-    data: "",
-            options: {
-                scales: {
-                yAxes: [{
-                label: "Severity",
-    borderColor: window.chartColor.red,
-    backgroundColor: window.chartColor.red,
-    fill: false,
-    data: [],
-    yaxisID: y - axis - 1,
-},
-                    {
-                label: "Temperature",
-    borderColor: window.chartColor.blue,
-    backgroundColor: window.chartColor.blue,
-    fill: false,
-    data: [],
-    yaxisID: y - axis - 2,
 
-},
-                    {
-                label: "Humidity",
-    borderColor: window.chartColor.green,
-    backgroundColor: window.chartColor.green,
-    fill: false,
-    data: [],
-    yaxisID: y - axis - 3,
-                    }, {
-                label: "Activity Level",
-    borderColor: window.chartColor.black,
-    backgroundColor: window.chartColor.black,
-    fill: false,
-    data: [],
-    yaxisID: y - axis - 4,
-}
-]
-}
-}
-        }, ctx, {
-                type: "",
-    data: "",
-            options: {
-                scales: {
-                xAxes: [{
-                type: "category",
-    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
-}]
-}
-}
-})
-
-</div>
-    )
-}
 export default Graph;
