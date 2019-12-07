80a8ca83e75f3b131f9e8c0424e8c64b2008bdde
Nik Arboleda <nik.arboleda@gmail.com>
2019-12-07 12:07:46 -0500

    test2

---
+++ client/build/precache-manifest.c88c27d70200304b7383315c3ca0d332.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f204d87a990aebb83dd9dbc92abfcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d668b134a7a64ba948",
+    "url": "/static/css/main.6edf1044.chunk.css"
+  },
+  {
+    "revision": "d648832b0069e8fd3f9e",
+    "url": "/static/js/2.77d097df.chunk.js"
+  },
+  {
+    "revision": "89d668b134a7a64ba948",
+    "url": "/static/js/main.f0aaee1c.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "f1b977f81a25e221197794cb344aba5f",
+    "url": "/static/media/healthbuddyBackground.f1b977f8.png"
+  },
+  {
+    "revision": "7bfdc840029368a580dd06f6d6bc46fd",
+    "url": "/static/media/healthbuddyBearHead.7bfdc840.png"
+  },
+  {
+    "revision": "63b63da7d578b02e92bc93c3fed47c13",
+    "url": "/static/media/healthbuddyBearHeadRed.63b63da7.png"
+  }
+]);
