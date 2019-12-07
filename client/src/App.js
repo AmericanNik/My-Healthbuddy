68c0161ae0128738ef5eb2497a68377e7339b88f
Nik Arboleda <nik.arboleda@gmail.com>
2019-12-07 13:50:49 -0500

    try and try again

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Graph from './components/graph/graph';
-import Logs from './components/auth/Logs';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Footer from './components/layout/Footer';
-import ConditionSearchBar from './components/conditionSearchBar/ConditionSearchBar';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Graph from "./components/graph/graph";
+import Logs from "./components/auth/Logs";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Footer from "./components/layout/Footer";
+import ConditionSearchBar from "./components/conditionSearchBar/ConditionSearchBar";
+import "./App.css";
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
-      <Route exact path='/' component={Landing} />
+      <Route exact path="/" component={Landing} />
       <Switch>
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-        []{' '}
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/logs" component={Logs} />
+        <Route exact path="/graph" component={Graph} />
       </Switch>
-      <Route exact path='/logs' component={Logs} />
-      <Route exact path='/graph' component={Graph} />
       <ConditionSearchBar />
       <Footer />
     </Fragment>
